5518ef46cb1cb21e6c8bf38b9243aa9d902592a3
fmascena-dev <felipe.mascena.dev@gmail.com>
2025-08-26 22:39:17 -0300

    feat: create page Movies

---
+++ src/pages/Movies.vue
@@ -0,0 +1,16 @@
+<script></script>
+
+<template>
+    <h1>Filmes 🎥</h1>
+</template>
+
+<style scoped>
+h1 {
+    font-family: var(--font-two);
+    font-size: 4.5rem;
+    letter-spacing: 3px;
+    color: #FFF;
+    text-align: center;
+    margin-top: 2rem;
+}
+</style>
