4cdc34a4aff349f9f028e9099b1f4dccb41f4f79
fmascena-dev <felipe.mascena.dev@gmail.com>
2025-08-26 22:36:27 -0300

    feat(header): add Header component with logo and navigation

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,42 @@
-<script setup></script>
+<script setup>
+import Navbar from './Navbar.vue';
+
+</script>
 
 <template>
   <header>
     <img src="/logo.png" alt="logo filmes & series" />
+    <nav>
+      <Router-link to="/home">Home</Router-link>
+      <Router-link to="/movies">Filmes</Router-link>
+      <Router-link>Séries</Router-link>
+    </nav>
   </header>
+  <Navbar />
 </template>
 
 <style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 2rem;
+}
+
 img {
-  width: 20%;
-  /* padding: 1.5em; */
+  width: 25%;
+}
+
+nav {
+  display: flex;
+  justify-content: space-between;
+  width: 25%;
+}
+
+nav a {
+  text-decoration: none;
+  color: #FFF;
+  font-weight: bold;
+  font-size: 2rem;
 }
 </style>
